2781603e8ae0d982a756b99a33fafe6f699d268b
alextcf <alextcf@gmail.com>
2024-11-23 17:15:23 +0000

    Add City entity and related configurations
    - Updated `Country` class in `Country.cs` to include a constructor initializing `Cities` as an empty list and added `Cities` property of type `IEnumerable<City>`.
    - Added `DbSet<City>` property named `Cities` to `VigilantUmbrellaDbContext.cs`.
    - Updated `VigilantUmbrellaDbContextModelSnapshot.cs` to include `City` entity configuration, defining properties, foreign key relationship with `Country`, and navigation properties.
    - Added new `City` class in `City.cs` within `vigilant_umbrella_domain.Entities.Countries` namespace, including properties for `Code`, `Country`, and inheriting from `BaseEntity`.
    - Created migration file `20241123170655_Cities.cs` to add `Cities` table to the database, defining schema and foreign key constraint linking `CountryId` to `Countries` table.
    - Generated designer file `20241123170655_Cities.Designer.cs` to reflect migration changes, including entity configurations for `City` and `Country`.

--- src/vigilant-umbrella-infrastructure/Migrations/20241123170655_Cities.Designer.cs
+++ src/vigilant-umbrella-infrastructure/Migrations/20241123170655_Cities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using vigilant_umbrella_infrastructure.Context;
 
@@ -11,9 +12,11 @@ using vigilant_umbrella_infrastructure.Context;
 namespace vigilant_umbrella_infrastructure.Migrations
 {
     [DbContext(typeof(VigilantUmbrellaDbContext))]
-    partial class VigilantUmbrellaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241123170655_Cities")]
+    partial class Cities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
